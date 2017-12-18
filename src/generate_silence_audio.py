9455f12da9cb022f150228b2d07e32fb6846d606
Misha Zhukov <3v1l91l@gmail.com>
2017-12-18 14:46:18 +0200

    add more noise types

--- src/generate_silence_audio.py
+++ src/generate_silence_audio.py
@@ -2,12 +2,17 @@ from lib import *
 from scipy.io import wavfile
 import numpy as np
 import os
+from numpy.fft import rfft, irfft
 
 train_dir = os.path.join('..', 'input', 'train')
 back_noise_dir = os.path.join('..', 'input', 'train')
 silence_audio_dirs = [os.path.join(train_dir, 'test','silence'),
                       os.path.join(train_dir, 'valid','silence'),
                       os.path.join(train_dir, 'audio', 'silence')]
+state = np.random.RandomState()
+
+def to_16bit_audio(wav):
+    return np.int16(wav / np.max(np.abs(wav)) * 32767)
 
 def generate_and_save_silences(silence_audio_dir):
     if not os.path.exists(silence_audio_dir):
@@ -23,21 +28,44 @@ def generate_and_save_silences(silence_audio_dir):
     for i in idx:
         sec_length = (wavs[i].shape[0] // fs) - 1
         sec_start = np.random.randint(0, sec_length)
-        # multiplier = np.random.uniform()
-        multiplier = np.random.uniform(0.5, 2, 1)
-        gen_wav = multiplier * wavs[i][sec_start * fs : (sec_start+1) * fs]
+        gen_wav = wavs[i][sec_start * fs : (sec_start+1) * fs]
         wavfile.write(os.path.join(silence_audio_dir, 'noise_' + str(count) + '.wav'), fs, gen_wav)
         count += 1
 
-    for i in range(500):
-        gen_wav = np.array([0] * fs)
+    for i in range(200):
+        gen_wav = np.array([0] * fs, dtype=np.int16)
         wavfile.write(os.path.join(silence_audio_dir, 'noise_' + str(count) + '.wav'), fs, gen_wav)
         count += 1
 
-    for i in range(500):
-        # multiplier = np.random.uniform(0.1, 3, 1)
-        multiplier = 3
-        gen_wav = np.random.choice([-1, 0, 1], fs) * multiplier
+    for i in range(200):
+        gen_wav = np.random.choice([-16000, 0, 16000], fs)
+        wavfile.write(os.path.join(silence_audio_dir, 'noise_' + str(count) + '.wav'), fs, gen_wav)
+        count += 1
+
+    # blue_noise
+    for i in range(200):
+        X = state.randn(fs // 2 + 1) + 1j * state.randn(fs // 2 + 1)
+        S = np.sqrt(np.arange(len(X)))  # Filter
+        gen_wav = (irfft(X * S)).real
+        gen_wav = to_16bit_audio(gen_wav)
+        wavfile.write(os.path.join(silence_audio_dir, 'noise_' + str(count) + '.wav'), fs, gen_wav)
+        count += 1
+
+    # brown_noise
+    for i in range(200):
+        X = state.randn(fs // 2 + 1) + 1j * state.randn(fs // 2 + 1)
+        S = (np.arange(len(X)) + 1)  # Filter
+        gen_wav = (irfft(X / S)).real
+        gen_wav = to_16bit_audio(gen_wav)
+        wavfile.write(os.path.join(silence_audio_dir, 'noise_' + str(count) + '.wav'), fs, gen_wav)
+        count += 1
+
+    # violet_noise
+    for i in range(200):
+        X = state.randn(fs // 2 + 1) + 1j * state.randn(fs // 2 + 1)
+        S = (np.arange(len(X)))  # Filter
+        gen_wav = (irfft(X * S)).real
+        gen_wav = to_16bit_audio(gen_wav)
         wavfile.write(os.path.join(silence_audio_dir, 'noise_' + str(count) + '.wav'), fs, gen_wav)
         count += 1
 

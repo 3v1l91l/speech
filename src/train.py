d81d9a54421ea9a74859eab7bb9f46a53ea12e70
Misha Zhukov <3v1l91l@gmail.com>
2018-01-10 11:19:40 +0200

    m

--- src/train.py
+++ src/train.py
@@ -140,11 +140,11 @@ def train_model(binary_label):
     model.fit_generator(
         generator=train_gen,
         epochs=100,
-        steps_per_epoch=len(y_train) // BATCH_SIZE // 120,
+        steps_per_epoch=len(y_train) // BATCH_SIZE // 4,
         validation_data=valid_gen,
         validation_steps=len(y_valid) // BATCH_SIZE // 4,
         callbacks=get_callbacks(label_index, binary_label),
-        workers=4,
+        workers=24,
         use_multiprocessing=False,
         verbose=1
     )
